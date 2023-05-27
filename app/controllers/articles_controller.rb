c85650e2ce9135f4b0a20b594782c14f87909e40
chenyuan <cy476571@gmail.com>
2023-05-27 23:44:50 +0800

    add comment model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,40 @@ class ArticlesController < ApplicationController
     @article = Article.find_by_id(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def update
+    @article = Article.find_by_id(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find_by_id(params[:id])
+    @article.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  root "articles#index" # Homepage. This will point to the index action of the ArticlesController
+  resources :articles # This will generate all the routes for the ArticlesController
 end
