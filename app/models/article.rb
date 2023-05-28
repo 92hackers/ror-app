b3b076d7cbd24f4cd9d16688d260e83f86a90b7a
chenyuan <cy476571@gmail.com>
2023-05-28 18:52:58 +0800

    add comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments # Relationship with Comment model
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index" # Homepage. This will point to the index action of the ArticlesController
-  resources :articles # This will generate all the routes for the ArticlesController
+
+  # This will generate all the routes for the ArticlesController
+  resources :articles do
+    # This will generate all the routes for the CommentsController
+    # Sub route under articles
+    resources :comments
+  end
 end
